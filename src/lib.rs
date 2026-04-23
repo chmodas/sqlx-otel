00ab4edfd8c5904cfda5bdb091817caf128b99b1
Borislav Borisov <chmodas@astutebits.com>
2026-04-23 11:51:16 +0100

    chore: Enable pedantic clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-//! Lightweight SQLx wrapper that emits OpenTelemetry-native spans and metrics
+//! Lightweight `SQLx` wrapper that emits OpenTelemetry-native spans and metrics
 //! following the database client semantic conventions.
